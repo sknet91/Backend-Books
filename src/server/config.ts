c3c7a314e4ebec573e146105c003f5a2f5c45e00
German Mejia Vieco <germanmejiavieco@gmail.com>
2020-04-16 21:18:05 -0500

    first update JWT

--- src/server/config.ts
+++ src/server/config.ts
@@ -13,11 +13,14 @@ const startServer = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
+  // Port
+  app.set('port', process.env.PORT || 3000);
+
   // Carpeta Uploads
 
   routes(app);
-  app.listen(3001);
-  console.log("Listening on port: 3000");
+  app.listen(app.get('port'));
+  console.log("Listening on port: ", app.get('port'));
 };
 
 export default startServer;
